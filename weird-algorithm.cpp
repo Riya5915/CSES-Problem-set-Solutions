4e61cf7ddf7f4e1c229616826d6fddd5ac6c0cb9
Riya Sharma <sriya5915@gmail.com>
2020-10-25 15:28:24 +0530

    Weird Algorithm Solution
    Time limit: 1.00 s Memory limit: 512 MB
    Consider an algorithm that takes as input a positive integer n. If n is
    even, the algorithm divides it by two, and if n is odd, the algorithm
    multiplies it by three and adds one. The algorithm repeats this, until n
    is one. For example, the sequence for n=3 is as follows:
    3→10→5→16→8→4→2→1
    
    Your task is to simulate the execution of the algorithm for a given
    value of n.
    
    Input
    
    The only input line contains an integer n.
    
    Output
    
    Print a line that contains all values of n during the algorithm.
    
    Constraints
    1≤n≤106
    Example
    
    Input:
    3
    
    Output:
    3 10 5 16 8 4 2 1

--- weird-algorithm.cpp
+++ weird-algorithm.cpp
@@ -12,9 +12,9 @@ int main(int argc, char **argv){
     while(n != 1){
     
     	if(n % 2 == 0){
-	    n = n / 2;
-	} else {
-	    n = (n * 3) + 1;
+	        n = n / 2;
+        } else {
+            n = (n * 3) + 1;
 	}
 	
 	cout << n << " ";
