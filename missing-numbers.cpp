4955b764fc8140565f7ba0b270d9fbae9b70b050
Riya Sharma <sriya5915@gmail.com>
2020-10-25 13:48:06 +0530

    Time limit: 1.00 s Memory limit: 512 MB You are given all numbers between 1,2,…,n except one. Your task is to find the missing number.
    Input
    
    The first input line contains an integer n.
    
    The second line contains n−1 numbers. Each number is distinct and
    between 1 and n (inclusive).
    
    Output
    
    Print the missing number.
    
    Constraints
    2≤n≤2⋅105
    Example
    
    Input:
    5
    2 3 1 5
    
    Output:
    4

---
+++ missing-numbers.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char **argv){
+    long long n, s1 = 0, s2 = 0;
+    cin >> n;
+    if(n % 2 == 0)
+    	s1 = (n * (n + 1)) /2;
+    else
+    	s1 = n * ((n + 1) / 2);
+    
+    for(int i = 1; i < n; i++){
+    	long long a;
+    	cin >> a;
+    	s2 += a;
+	}
+	
+	cout << s1 - s2;
+    return 0;
+}
